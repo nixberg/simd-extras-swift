066d83cef4543869a65766ac11dd54c0eda3485c
nixberg <310825+nixberg@users.noreply.github.com>
2024-06-01 17:08:39 +0200

    Update Rotate.swift

--- Sources/SIMDExtras/Rotate.swift
+++ Sources/SIMDExtras/Rotate.swift
@@ -19,3 +19,25 @@ extension SIMD where Scalar: FixedWidthInteger & UnsignedInteger {
         self &<< (Scalar(Scalar.bitWidth) &- count) | self &>> count
     }
 }
+
+extension SIMD where Scalar: FixedWidthInteger & UnsignedInteger {
+    @inline(__always)
+    public mutating func rotate(left count: Self) {
+        self = self.rotated(left: count)
+    }
+    
+    @inline(__always)
+    public func rotated(left count: Self) -> Self {
+        self &<< count | self &>> (Scalar(Scalar.bitWidth) &- count)
+    }
+    
+    @inline(__always)
+    public mutating func rotate(right count: Self) {
+        self = self.rotated(right: count)
+    }
+    
+    @inline(__always)
+    public func rotated(right count: Self) -> Self {
+        self &<< (Scalar(Scalar.bitWidth) &- count) | self &>> count
+    }
+}
